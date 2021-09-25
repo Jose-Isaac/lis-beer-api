6c081db10d35d165f4d161e8ef73f8f853294a37
José Isaac <joseisaacbarreirocampos@gmail.com>
2021-09-25 16:47:44 -0300

    initial commit

---
+++ src/main/kotlin/com/lisbeer/LisBeerApplication.kt
@@ -0,0 +1,11 @@
+package com.lisbeer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LisBeerApplication
+
+fun main(args: Array<String>) {
+	runApplication<LisBeerApplication>(*args)
+}
---
+++ src/test/kotlin/com/lisbeer/LisBeerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lisbeer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LisBeerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
